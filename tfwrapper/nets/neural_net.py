b3d9d34be309f6026b5db993cd95c99c847a503e
Esten Leonardsen <estenleonardsen@gmail.com>
2017-03-15 17:06:37 +0100

    Testing

--- tfwrapper/nets/neural_net.py
+++ tfwrapper/nets/neural_net.py
@@ -23,10 +23,10 @@ class NeuralNet(SupervisedModel):
 		self.pred = prev
 
 		print('Loss: ' + str(sess))
-		self.loss = self.loss_function()
-		self.optimizer = self.optimizer_function()
+		self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.pred, labels=self.y, name=self.name + '_softmax'), name=self.name + '_loss')
+		self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate, name=self.name + '_adam').minimize(self.loss, name=self.name + '_optimizer')
 		self.correct_pred = tf.equal(tf.argmax(self.pred, 1), tf.argmax(self.y, 1), name=name + '_correct_pred')
-		self.accuracy = self.accuracy_function()
+		self.accuracy = tf.reduce_mean(tf.cast(self.correct_pred, tf.float32), name=self.name + '_accuracy')
 
 		self.graph = graph
 
@@ -45,6 +45,35 @@ class NeuralNet(SupervisedModel):
 		fc = tf.nn.relu(fc, name=name)
 
 		return fc
+
+	def train(self, X, y, val_X=None, val_y=None, validate=True, epochs=5000, sess=None, verbose=False):
+		print('TRAINING NEURAL NET')
+		assert len(X) == len(y)
+
+		X = np.reshape(X, [-1] + self.X_shape)
+		y = np.reshape(y, [-1, self.y_size])
+		if val_X is None and validate:
+			X, y, val_X, val_y = split_dataset(X, y)
+			
+		X_batches = self.batch_data(X)
+		y_batches = self.batch_data(y)
+		num_batches = len(X_batches)
+		
+		if verbose:
+			print('Training ' + self.name + ' with ' + str(len(X)) + ' cases')
+
+		sess.run(tf.global_variables_initializer())
+		for epoch in range(epochs):
+			for i in range(num_batches):
+				sess.run(self.optimizer, feed_dict={self.X: X_batches[i], self.y: y_batches[i]})
+
+			if verbose:			
+				loss, acc = sess.run([self.loss, self.accuracy], feed_dict={self.X: X[-1000:], self.y: y[-1000:]})
+				print('Epoch %d, train loss: %.3f, train acc: %2f' % (epoch + 1, loss, acc))
+
+				if validate:
+					loss, acc = sess.run([self.loss, self.accuracy], feed_dict={self.X: val_X, self.y: val_y})
+					print('Epoch %d, val loss: %.3f, val acc: %2f' % (epoch + 1, loss, acc))
 
 	def load(self, filename, sess=None):
 		if sess is None:
