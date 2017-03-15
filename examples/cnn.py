24e06c8ebd9be1aa1c18a47d8a25aa5ea2accc91
Esten Leonardsen <estenleonardsen@gmail.com>
2017-03-15 09:52:54 +0100

    Added example for CNN

--- examples/cnn.py
+++ examples/cnn.py
@@ -8,21 +8,21 @@ def generate_dataset(size=1000):
 	X = []
 	y = []
 
-	for i in range(0, size/2):
-		X.append(np.ones((48, 48)))
+	for i in range(0, int(size/2)):
+		X.append(np.ones((48, 48, 1)))
 		y.append(1)
 
-	for i in range(0, size/2):
-		X.append(np.zeros((48, 48)))
+	for i in range(0, int(size/2)):
+		X.append(np.zeros((48, 48, 1)))
 		y.append(0)
 
 	return Dataset(X=np.asarray(X), y=np.asarray(y))
 
 dataset = generate_dataset()
-X, y, test_X, test_y = dataset.getdata(shuffle=True, onehot=True, split=True)
+X, y, test_X, test_y, _ = dataset.getdata(shuffle=True, onehot=True, split=True)
 
 graph = tf.Graph()
 with graph.as_default():
 	with tf.Session(graph=graph) as sess:
-		cnn = DeepCNN([48, 48], 2, sess=sess, graph=graph, name='ExampleCNN')
+		cnn = DeepCNN([48, 48, 1], 2, sess=sess, graph=graph, name='ExampleCNN')
 		cnn.train(X, y, epochs=100, verbose=True)
