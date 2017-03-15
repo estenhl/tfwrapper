015b443f4556062507789f7c853e1bed2d77f067
Esten Leonardsen <estenleonardsen@gmail.com>
2017-03-15 11:57:32 +0100

    Tweaked cats and dogs dataset

--- examples/cnn.py
+++ examples/cnn.py
@@ -3,28 +3,16 @@ import tensorflow as tf
 
 from tfwrapper import Dataset
 from tfwrapper.nets import DeepCNN
+from tfwrapper.datasets import cats_and_dogs
 
-def generate_dataset(size=1000):
-	X = []
-	y = []
 
-	for i in range(0, int(size/2)):
-		X.append(np.ones((48, 48, 1)))
-		y.append(1)
-
-	for i in range(0, int(size/2)):
-		X.append(np.zeros((48, 48, 1)))
-		y.append(0)
-
-	return Dataset(X=np.asarray(X), y=np.asarray(y))
-
-dataset = generate_dataset()
-X, y, test_X, test_y, _ = dataset.getdata(shuffle=True, onehot=True, split=True)
+dataset = cats_and_dogs()
+X, y, test_X, test_y, _ = dataset.getdata(shuffle=True, translate_labels=True, onehot=True, split=True)
 
 graph = tf.Graph()
 with graph.as_default():
 	with tf.Session(graph=graph) as sess:
-		cnn = DeepCNN([48, 48, 1], 2, sess=sess, graph=graph, name='ExampleCNN')
+		cnn = DeepCNN([196, 196, 3], 2, sess=sess, graph=graph, name='ExampleCNN')
 		cnn.train(X, y, epochs=10, sess=sess, verbose=True)
 		_, acc = cnn.validate(test_X, test_y, sess=sess)
 		print('Accuracy for example CNN: %.2f' % acc)
---
+++ tfwrapper/datasets/__init__.py
@@ -0,0 +1,69 @@
+import os
+import cv2
+import zipfile
+from tfwrapper import Dataset
+
+curr_path = os.path.dirname(os.path.realpath(__file__))
+
+def download_cats_and_dogs(verbose=False):
+	root_path = os.path.join(curr_path, 'cats_and_dogs')
+	if not os.path.isdir(root_path):
+		os.mkdir(root_path)
+
+	data_path = os.path.join(root_path, 'data')
+	if not os.path.isdir(data_path):
+		os.mkdir(data_path)
+
+	labels_file = os.path.join(root_path, 'labels.txt')
+	num_refined_files = len(os.listdir(data_path))
+	num_raw_files = 0
+	if os.path.isdir(os.path.join(root_path, 'train')):
+		num_raw_files = len(os.listdir(os.path.join(root_path, 'train')))
+	
+	if num_refined_files < 25000:
+		local_zip = os.path.join(root_path, 'train.zip')
+		if not os.path.isfile(local_zip):
+			print('Downloading dataset')
+			raise NotImplementedError('Unable to download zip')
+
+		if num_raw_files < 25000:
+			with zipfile.ZipFile(local_zip, 'r') as f:
+				print('Extracting dataset')
+				f.extractall(root_path)
+
+		print('Refining dataset')
+		with open(labels_file, 'w') as f:
+			for filename in os.listdir(os.path.join(root_path, 'train')):
+				src_file = os.path.join(root_path, 'train', filename)
+
+				if not filename.endswith('.jpg'):
+					os.remove(src_file)
+					continue
+
+				pref, midf, _ = filename.split('.')
+				dest = pref + '_' + midf + '.jpg'
+				dest_file = os.path.join(data_path, dest)
+
+				if filename.startswith('cat'):
+					f.write('cat,' + dest + '\n')
+				elif filename.startswith('dog'):
+					f.write('dog,' + dest + '\n')
+				else:
+					os.remove(src_file)
+					continue
+
+				img = cv2.imread(src_file)
+				img = cv2.resize(img, (196, 196))
+				cv2.imwrite(dest_file, img)
+				os.remove(src_file)
+
+		os.rmdir(os.path.join(root_path, 'train'))
+		if os.path.isfile(os.path.join(root_path, 'train.zip')):
+			os.remove(os.path.join(root_path, 'train.zip'))
+	
+	return data_path, labels_file
+
+def cats_and_dogs(verbose=False):
+	data_path, labels_file = download_cats_and_dogs(verbose=verbose)
+	dataset = Dataset(root_folder=data_path, labels_file=labels_file, verbose=verbose)
+	return dataset
