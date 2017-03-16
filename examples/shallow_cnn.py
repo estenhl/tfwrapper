bcd70b736bd5b94c3c8e34d3057148d2906e4725
Esten Leonardsen <estenleonardsen@gmail.com>
2017-03-16 10:37:47 +0100

    Reverted

--- examples/shallow_cnn.py
+++ examples/shallow_cnn.py
@@ -3,16 +3,16 @@ import tensorflow as tf
 
 from tfwrapper import Dataset
 from tfwrapper.nets import ShallowCNN
-from tfwrapper.datasets import cats_and_dogs
+from tfwrapper.datasets import mnist
 
 
-dataset = cats_and_dogs()
+dataset = mnist(verbose=True)
 X, y, test_X, test_y, _ = dataset.getdata(shuffle=True, translate_labels=True, onehot=True, split=True)
 
 graph = tf.Graph()
 with graph.as_default():
 	with tf.Session(graph=graph) as sess:
-		cnn = ShallowCNN([192, 192, 3], 2, sess=sess, graph=graph, name='ExampleShallowCNN')
-		cnn.train(X[:-1000], y[:-1000], val_X=X[-1000:], val_y=y[-1000:], epochs=500, sess=sess, verbose=True)
-		#_, acc = cnn.validate(test_X, test_y, sess=sess)
-		#print('Accuracy for example CNN: %.2f' % acc)
+		cnn = ShallowCNN([28, 28, 1], 2, sess=sess, graph=graph, name='ExampleShallowCNN')
+		cnn.train(X, y, epochs=500, sess=sess, verbose=True)
+		_, acc = cnn.validate(test_X, test_y, sess=sess)
+		print('Accuracy for example CNN: %.2f' % acc)
--- tfwrapper/datasets/__init__.py
+++ tfwrapper/datasets/__init__.py
@@ -1,11 +1,17 @@
 import os
 import cv2
+import gzip
 import zipfile
+import urllib.request
+
+from struct import unpack
+from numpy import zeros, uint8, float32
+
 from tfwrapper import Dataset
 
 curr_path = os.path.dirname(os.path.realpath(__file__))
 
-def download_dataset(name):
+def setup_structure(name):
 	root_path = os.path.join(curr_path, name)
 	if not os.path.isdir(root_path):
 		os.mkdir(root_path)
@@ -18,8 +24,14 @@ def download_dataset(name):
 
 	return root_path, data_path, labels_file
 
+def download_file(url, path, verbose=False):
+	if verbose:
+		print('Downloading ' + url  + ' to ' + path)
+
+	urllib.request.urlretrieve(url, path)
+
 def download_cats_and_dogs(verbose=False):
-	root_path, data_path, labels_file = download_dataset('cats_and_dogs')
+	root_path, data_path, labels_file = setup_structure('cats_and_dogs')
 
 	num_refined_files = len(os.listdir(data_path))
 	num_raw_files = 0
@@ -69,7 +81,65 @@ def download_cats_and_dogs(verbose=False):
 	
 	return data_path, labels_file
 
+def parse_mnist_data(data_file, labels_file, verbose=False):
+	if verbose:
+		print('Unpacking mnist data')
+
+	images = gzip.open(data_file, 'rb')
+	labels = gzip.open(labels_file, 'rb')
+
+	images.read(4)
+	number_of_images = images.read(4)
+	number_of_images = unpack('>I', number_of_images)[0]
+	rows = images.read(4)
+	rows = unpack('>I', rows)[0]
+	cols = images.read(4)
+	cols = unpack('>I', cols)[0]
+
+	labels.read(4)
+	N = labels.read(4)
+	N = unpack('>I', N)[0]
+
+	X = zeros((N, rows, cols), dtype=float32)
+	y = zeros((N, 1), dtype=uint8)
+	for i in range(N):
+		if i % 1000 == 0 and verbose:
+			print("Read %i of %i images" % (i, number_of_images))
+		for row in range(rows):
+			for col in range(cols):
+				tmp_pixel = images.read(1)
+				tmp_pixel = unpack('>B', tmp_pixel)[0]
+				X[i][row][col] = tmp_pixel
+		tmp_label = labels.read(1)
+		y[i] = unpack('>B', tmp_label)[0]
+
+	return X, y
+
+def download_mnist(verbose=False):
+	data_url = 'http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz'
+	labels_url = 'http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz'
+
+	root_path, data_path, labels_file = setup_structure('mnist')
+	local_data_zip = os.path.join(root_path, 'data.zip')
+	local_labels_zip = os.path.join(root_path, 'labels.zip')
+
+	num_files = len(os.listdir(data_path))
+	if True: #num_files < zzz
+		if not os.path.isfile(local_data_zip):
+			download_file(data_url, local_data_zip, verbose=verbose)
+		if not os.path.isfile(local_labels_zip):
+			download_file(labels_url, local_labels_zip, verbose=verbose)
+
+	X, y = parse_mnist_data(local_data_zip, local_labels_zip, verbose=verbose)
+	
+	return X, y
+
 def cats_and_dogs(verbose=False):
 	data_path, labels_file = download_cats_and_dogs(verbose=verbose)
 	dataset = Dataset(root_folder=data_path, labels_file=labels_file, verbose=verbose)
+	return dataset
+
+def mnist(verbose=False):
+	X, y = download_mnist(verbose=verbose)
+	dataset = Dataset(X=X, y=y)
 	return dataset
