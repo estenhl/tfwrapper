0e99ab3b2945e6e2d7d01ccb88a764f6514eba8e
Esten Leonardsen <estenleonardsen@gmail.com>
2017-04-10 15:29:26 +0200

    Removed PCA stuff

--- examples/retrain_inceptionv3.py
+++ examples/retrain_inceptionv3.py
@@ -8,14 +8,14 @@ from tfwrapper.datasets import checkboxes
 
 from utils import curr_path
 
-inception_path = os.path.join(curr_path, 'data', 'inception_v3_features.csv')
+features_path = os.path.join(curr_path, 'data', 'checkbox_features.csv')
 model_path = os.path.join(curr_path, 'data', 'inception')
 if not os.path.isdir(os.path.join(curr_path, 'data')):
 	os.mkdir(os.path.join(curr_path, 'data'))
 
 _, data_folder = checkboxes()
 inception = InceptionV3()
-features = inception.extract_features_from_datastructure(data_folder, feature_file=inception_path)
+features = inception.extract_features_from_datastructure(data_folder, feature_file=features_path)
 
 dataset = Dataset(features=features)
 X, y, test_X, test_y, labels =dataset.getdata(normalize=True, balance=True, translate_labels=True, shuffle=True, onehot=True, split=True)
