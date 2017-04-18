8c33c9e50ce465ed44cb1994c6cfad8d3e729bc7
Stian Lind Petlund <stianlp@gmail.com>
2017-04-18 09:49:10 +0200

    Added setup.py to make tfwrapper installable from pip

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='tfwrapper',
+    version='0.0.2',
+    description='Wrapper for tfwrapper',
+    url='https://github.com/epigramai/',
+    author='Esten Høyland Leonardsen',
+    author_email='esten@epigram.ai',
+    # license='<license>',
+    packages=['tfwrapper'],
+    install_requires=['tensorflow>=0.11.0'],
+)
