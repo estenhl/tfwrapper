06bc2998b6f5c5f653f037c0f3f228b22193efcf
Stian Lind Petlund <stianlp@gmail.com>
2017-04-18 10:29:56 +0200

    Minor change to setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='tfwrapper',
     version='0.0.2',
     description='Wrapper for tfwrapper',
-    url='https://github.com/epigramai/',
+    url='https://github.com/epigramai/tfwrapper',
     author='Esten Høyland Leonardsen',
     author_email='esten@epigram.ai',
     # license='<license>',
