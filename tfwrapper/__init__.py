1fed3556c255caa5d94b89662d42639467fe9ca6
Esten Leonardsen <estenleonardsen@gmail.com>
2017-04-18 15:52:31 +0200

    Put in a feature extractor preprocessor

--- tfwrapper/__init__.py
+++ tfwrapper/__init__.py
@@ -1,5 +1,6 @@
 from .dataset import Dataset
 from .dataset import ImageDataset
 from .dataset import ImagePreprocess
+from .dataset import FeatureExtractor
 from .supervisedmodel import TFSession
 from .supervisedmodel import SupervisedModel
