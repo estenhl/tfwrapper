3801fa5508a544d15a3ad41d052fefc3c78dcb13
Esten Leonardsen <estenleonardsen@gmail.com>
2017-04-20 10:13:09 +0200

    Defaults CNN learning rate to the empirically tested (N=1) 0.001

--- tfwrapper/nets/cnn.py
+++ tfwrapper/nets/cnn.py
@@ -6,6 +6,8 @@ from tfwrapper.utils.exceptions import InvalidArgumentException
 from .neural_net import NeuralNet
 
 class CNN(NeuralNet):
+	learning_rate = 0.001
+	
 	def __init__(self, X_shape, classes, layers, sess=None, name='NeuralNet'):
 		with TFSession(sess) as sess:
 			super().__init__(X_shape, classes, layers, sess=sess, name=name)
