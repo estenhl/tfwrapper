1b6226c1272b94770dd8e99515d85b4c92aca7bf
Esten Leonardsen <estenleonardsen@gmail.com>
2017-04-24 13:40:58 +0200

    Added activation visualization

---
+++ tfwrapper/visualization/activation.py
@@ -0,0 +1,27 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+
+from tfwrapper.utils.exceptions import InvalidArgumentException
+
+def visualize_activation(features, img, cmap='YlOrRd'):
+    if not len(features.shape) == 3:
+        raise InvalidArgumentException('Requires features on the form (height, width, depth)')
+
+    height, width, depth = features.shape
+    weights = features.sum(axis=2)
+    weights = weights / np.amax(weights)
+
+    height, width, _ = img.shape
+    scaled_weights = np.zeros((height, width))
+    horizontal_step = height / features.shape[0]
+    vertical_step = width / features.shape[1]
+    for i in range(height):
+        for j in range(width):
+            scaled_weights[i][j] = weights[int(i / horizontal_step)][int(j / vertical_step)]
+
+    figure = plt.figure(figsize=(10, 10))
+    ax = figure.add_subplot(111)
+    plt.axis('off')
+    ax.imshow(img)
+    ax.imshow(scaled_weights, alpha=0.25, cmap=cmap)
+    plt.show()
