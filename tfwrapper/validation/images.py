db5c2f120f968de0d6106503e10415fb121ad0f5
Esten Leonardsen <estenleonardsen@gmail.com>
2017-05-02 10:30:31 +0200

    Fixed X/Y relationship in imagesize validator

--- tfwrapper/validation/images.py
+++ tfwrapper/validation/images.py
@@ -8,13 +8,14 @@ from tfwrapper.nets import ShallowCNN
 from .validation import Validator
 from .validation import kfold_validation
 
+# Imagesizes are on the form (x, y)
 def kfold_validation_imagesize(dataset, image_sizes, k=10, epochs=10):
     validator = Validator([str(s) for s in image_sizes])
     for image_size in image_sizes:
         preprocessor = ImagePreprocessor()
         preprocessor.resize_to = image_size
         dataset.loader = ImageLoader(preprocessor)
-        create_model = lambda x, sess: ShallowCNN([image_size[0], image_size[1], 3], 2, name='Val%dx%d_%d' % (image_size[0], image_size[1], x), sess=sess)
+        create_model = lambda x, sess: ShallowCNN([image_size[1], image_size[0], 3], 2, name='Val%dx%d_%d' % (image_size[0], image_size[1], x), sess=sess)
         
         validator = kfold_validation(dataset, create_model, k=k, epochs=epochs, validator=validator)
 
