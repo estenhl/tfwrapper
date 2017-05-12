47654e65d8604009c2208a96e35bcb1822d03e7a
Esten Leonardsen <estenleonardsen@gmail.com>
2017-05-12 10:39:53 +0200

    Fixed inception tests which gave error msgs

--- tests/test_inception_v3.py
+++ tests/test_inception_v3.py
@@ -7,31 +7,40 @@ from tfwrapper.nets.pretrained import InceptionV3
 from utils import curr_path
 
 cat_img = os.path.join(curr_path, 'data', 'cat.jpg')
-inception = InceptionV3()
-sess = tf.Session()
 
-def test_from_file():
-    features = inception.extract_features_from_file(cat_img, sess=sess)
+class TestInceptionV3():
 
-    assert (1, 1, 2048) == features.shape
+    @classmethod
+    def setup_class(cls):
+        cls.sess = tf.Session()
+        cls.inception = InceptionV3()
 
-def test_from_data():
-    img = cv2.imread(cat_img)
-    features = inception.extract_features(img, sess=sess)
+    @classmethod
+    def teardown_class(cls):
+        cls.sess.close()
 
-    assert (1, 1, 2048) == features.shape
+    def test_from_file(self):
+        features = self.inception.extract_features_from_file(cat_img, sess=self.sess)
 
-def test_from_op():
-    img = cv2.imread(cat_img)
-    features = inception.run_op(InceptionV3.DEFAULT_FEATURES_LAYER, InceptionV3.DEFAULT_INPUT_LAYER, img, sess=sess)
+        assert (1, 1, 2048) == features.shape
 
-    assert (1, 1, 2048) == features.shape
+    def test_from_data(self):
+        img = cv2.imread(cat_img)
+        features = self.inception.extract_features(img, sess=self.sess)
 
-def test_bottleneck():
-    img = cv2.imread(cat_img)
-    features = inception.extract_bottleneck_features(img, sess=sess)
+        assert (1, 1, 2048) == features.shape
 
-    assert (2048, ) == features.shape
+    def test_from_op(self):
+        img = cv2.imread(cat_img)
+        features = self.inception.run_op(InceptionV3.DEFAULT_FEATURES_LAYER, InceptionV3.DEFAULT_INPUT_LAYER, img, sess=self.sess)
+
+        assert (1, 1, 2048) == features.shape
+
+    def test_bottleneck(self):
+        img = cv2.imread(cat_img)
+        features = self.inception.extract_bottleneck_features(img, sess=self.sess)
+
+        assert (2048, ) == features.shape
 
 
     
--- tests/test_inception_v4.py
+++ tests/test_inception_v4.py
@@ -7,31 +7,40 @@ from tfwrapper.nets.pretrained import InceptionV4
 from utils import curr_path
 
 cat_img = os.path.join(curr_path, 'data', 'cat.jpg')
-inception = InceptionV4()
-sess = tf.Session()
 
-def test_from_file():
-	features = inception.extract_features_from_file(cat_img, sess=sess)
+class TestInceptionV4():
 
-	assert (1536, ) == features.shape
+    @classmethod
+    def setup_class(cls):
+        cls.sess = tf.Session()
+        cls.inception = InceptionV4()
 
-def test_from_data():
-	img = cv2.imread(cat_img)
-	features = inception.extract_features(img, sess=sess)
+    @classmethod
+    def teardown_class(cls):
+        cls.sess.close()
 
-	assert (1536, ) == features.shape
+    def test_from_file(self):
+    	features = self.inception.extract_features_from_file(cat_img, sess=self.sess)
 
-def test_from_op():
-    img = cv2.imread(cat_img)
-    features = inception.run_op(InceptionV4.DEFAULT_FEATURES_LAYER, InceptionV4.DEFAULT_INPUT_LAYER, img, sess=sess)
+    	assert (1536, ) == features.shape
 
-    assert (1536, ) == features.shape
+    def test_from_data(self):
+    	img = cv2.imread(cat_img)
+    	features = self.inception.extract_features(img, sess=self.sess)
 
-def test_bottleneck():
-    img = cv2.imread(cat_img)
-    features = inception.extract_bottleneck_features(img, sess=sess)
+    	assert (1536, ) == features.shape
 
-    assert (1536, ) == features.shape
+    def test_from_op(self):
+        img = cv2.imread(cat_img)
+        features = self.inception.run_op(InceptionV4.DEFAULT_FEATURES_LAYER, InceptionV4.DEFAULT_INPUT_LAYER, img, sess=self.sess)
+
+        assert (1536, ) == features.shape
+
+    def test_bottleneck(self):
+        img = cv2.imread(cat_img)
+        features = self.inception.extract_bottleneck_features(img, sess=self.sess)
+
+        assert (1536, ) == features.shape
 
 
 	
