4e355642a88e878eeec953bff266e56138d4122b
Esten Leonardsen <estenleonardsen@gmail.com>
2017-06-01 16:03:40 +0200

    Added finetuning example

---
+++ examples/finetune_resnet50.py
@@ -0,0 +1,28 @@
+import numpy as np
+import tensorflow as tf
+
+from tfwrapper.nets.pretrained import PretrainedResNet50
+from tfwrapper.layers import randomized_preprocessing
+from tfwrapper.layers import vgg_preprocessing
+from tfwrapper.datasets import cifar10
+from tfwrapper.hyperparameters import adjust_after_epoch
+
+train = cifar10()
+train = train.balance()
+train = train.shuffle()
+train = train.translate_labels()
+train = train.onehot()
+
+test = cifar10(test=True)
+test = test.translate_labels()
+test = test.onehot()
+
+with tf.Session() as sess:
+    name = 'FinetunedResNet50'
+    preprocessing = randomized_preprocessing(name=name) + vgg_preprocessing(name=name)
+    cnn = PretrainedResNet50([32, 32, 3], 10, name='ExampleResNet50', sess=sess)
+    cnn.learning_rate = adjust_after_epoch(50, before=0.00005, after=0.00001)
+    cnn.batch_size = 256
+    cnn.train(train.X, train.y, validate=0.05, epochs=150, sess=sess)
+    _, acc = cnn.validate(test.X, test.y, sess=sess)
+    print('Test accuracy: %d%%' % (acc*100))
