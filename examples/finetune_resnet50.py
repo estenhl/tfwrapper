9cd06a6aadbd89fb9f1c21863eae80621409c0a3
Esten Leonardsen <estenleonardsen@gmail.com>
2017-06-02 09:51:05 +0200

    Added seed to dataset shuffle

--- examples/finetune_resnet50.py
+++ examples/finetune_resnet50.py
@@ -21,8 +21,8 @@ with tf.Session() as sess:
     name = 'FinetunedResNet50'
     preprocessing = randomized_preprocessing(name=name)
     cnn = PretrainedResNet50([32, 32, 3], 10, name='FinetunedResNet50', sess=sess)
-    cnn.learning_rate = adjust_at_epochs([5, 10, 20, 50], [0.001, 0.0005, 0.0001, 0.00005, 0.00001])
+    cnn.learning_rate = adjust_at_epochs([20, 50, 100], [0.00005, 0.00003, 0.00001, 0.000005])
     cnn.batch_size = 256
-    cnn.train(train.X, train.y, validate=0.05, epochs=150, sess=sess)
+    cnn.train(train.X, train.y, validate=0.05, epochs=500, sess=sess)
     _, acc = cnn.validate(test.X, test.y, sess=sess)
     print('Test accuracy: %d%%' % (acc*100))
