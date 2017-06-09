6c8bd08fbb49936802ae91d05c46800aa048fdfb
stiansel <stianselbek@gmail.com>
2017-06-09 12:01:21 +0200

    Added DatasetSamplingGenerator to init

--- tfwrapper/dataset/__init__.py
+++ tfwrapper/dataset/__init__.py
@@ -1,6 +1,6 @@
 from .dataset import Dataset, parse_folder_with_labels_file, parse_datastructure
 from .image_dataset import ImageDataset
-from .dataset_generator import DatasetGenerator
+from .dataset_generator import DatasetGenerator, DatasetSamplingGenerator
 from .image_preprocessor import ImagePreprocessor
 from .image_loader import ImageLoader
 from .feature_loader import FeatureLoader
