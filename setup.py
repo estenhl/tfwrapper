7a49c3f99582b144c352b130dfdee421ea732077
Esten Leonardsen <estenleonardsen@gmail.com>
2017-06-19 14:16:57 +0200

    Updated versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tfwrapper',
-    version='0.1.0-rc2',
+    version='0.1.0-rc3',
     description='Wrapper for tensorflow',
     url='https://github.com/epigramai/tfwrapper',
     author='Esten Høyland Leonardsen',
--- tfwrapper/__init__.py
+++ tfwrapper/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.0-rc2'
+__version__ = '0.1.0-rc3'
 
 from .logger import logger
 from .config import config
